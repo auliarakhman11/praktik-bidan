5d6ab8c9feb0c6531c58f06b4550b665c58b0f5b
auliarakhman11 <auliarakhman234@gmail.com>
2020-09-06 20:21:43 +0800

    install dan config auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('role:admin')->get('/dashboard', function(){
+    return 'Dashboard';
+})->name('dashboard');
